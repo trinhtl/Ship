ef5ae546a872f3779ad621244aaccd1545fab934
Tong Ly Trinh (VTI.D5) <trinh.tongly@vti.vn>
2019-04-11 12:04:09 +0700

    list view

---
+++ app/src/main/java/com/example/btl/ListPackages.java
@@ -0,0 +1,13 @@
+package com.example.btl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListPackages extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_packages);
+    }
+}
